41dcbd414b6a1a8f2e5acc56e191fb979c837ee0
b8511 <your@email.com>
2026-01-23 17:02:51 +0000

    add lock in

--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -6,6 +6,14 @@ function ItemsList({ items, deleteItem, updateItem }: ItemsListProps) {
   return (
     <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-2xl mt-6">
       <h2 className="text-2xl font-bold text-gray-800 mb-4">Items List</h2>
+      <div className="flex justify-end">
+        <button
+          className="mb-1 px-6 py-2 bg-blue-800 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors "
+          onClick={() => {}}
+        >
+          Lock In
+        </button>
+      </div>
       <div className="space-y-2">
         {items.map((item: Item) => (
           <ItemRow
