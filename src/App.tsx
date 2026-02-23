6da09085f0d7e9753ed0c41069ef4cb79bc4be98
b8511 <bruno8511gomes@gmail.com>
2026-02-23 20:02:49 +0000

    fix: remove hardcoded initial items from state

--- src/App.tsx
+++ src/App.tsx
@@ -44,33 +44,7 @@ function App() {
       }
     }
   };
-  const [items, setItems] = useState<Item[]>([
-    { id: 1, name: "Antwerp 2022 Challengers Autograph Capsule", count: 1 },
-    { id: 2, name: "Antwerp 2022 challengers sticker Capsule", count: 5 },
-    { id: 3, name: "Antwerp 2022 Champions Autograph Capsule", count: 2 },
-    { id: 4, name: "Antwerp 2022 Contenders Autograph Capsule", count: 49 },
-    { id: 5, name: "Antwerp 2022 Contenders Sticker Capsule", count: 41 },
-    { id: 6, name: "Antwerp 2022 Legends Autograph Capsule", count: 1 },
-    { id: 7, name: "Antwerp 2022 Legends stickers Capsule", count: 2 },
-    { id: 8, name: "Paris 2023 Challengers Autograph Capsule", count: 2 },
-    { id: 9, name: "Paris 2023 Challengers Stickers Capsule", count: 20 },
-    { id: 10, name: "Paris 2023 Champions Autograph Capsule", count: 2 },
-    { id: 11, name: "Paris 2023 Contenders Autograph Capsule", count: 24 },
-    { id: 12, name: "Paris 2023 Contenders Stickers Capsule", count: 14 },
-    { id: 13, name: "Paris 2023 Legends Autograph Capsule", count: 19 },
-    { id: 14, name: "Paris 2023 Legends Stickers Capsule", count: 4 },
-    { id: 15, name: "Sticker Capsule 2", count: 1 },
-    { id: 16, name: "Stockholm 2021 Challengers Stickers Capsule", count: 8 },
-    { id: 17, name: "Stockholm 2021 Champions Autograph Capsule", count: 2 },
-    { id: 18, name: "Stockholm 2021 Contenders Sticker Capsule", count: 8 },
-    { id: 19, name: "Stockholm 2021 Finalists Autograph Capsule", count: 3 },
-    { id: 20, name: "Stockholm 2021 Legends Patch Capsule", count: 1 },
-    { id: 21, name: "Stockholm 2021 Legends Stickers Capsule", count: 7 },
-    { id: 22, name: "Dreams & Nightmares Case", count: 32 },
-    { id: 23, name: "Clutch Case", count: 114 },
-    { id: 24, name: "Prisma Case", count: 127 },
-    { id: 25, name: "CS20 Case", count: 11 },
-  ]);
+  const [items, setItems] = useState<Item[]>([]);
   console.log("Current items:", items);
 
   const addItem = (name: string, count: number) => {
